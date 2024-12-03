d8350a1dc327f57c78d3e194b6c1e55c5d440abf
kanary <kanary@adminui-MacBookPro.local>
2024-12-03 22:03:17 +0900

    running code

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,64 @@
 use std::io::Read;
 
 fn main() {
-    println!("Hello, world!");
-
-    let code = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
+    let code = "++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>>++.>+.+++++++..+++.<<++++++++++++++.------------.>+++++++++++++++.>.+++.------.--------.<<+.>+++++++++++.";
     let mut pointer = 0;
     const MEMORY_SIZE: usize = 30000;
-    let mut memory = vec![0; MEMORY_SIZE];
+    let mut memory = vec![0u8; MEMORY_SIZE];
+    let code_chars: Vec<char> = code.chars().collect();
+    let mut c = 0;
+    let mut stack = Vec::new();
 
-    for c in code.chars() {
-        match c {
-            '+' => memory[pointer] += 1,
-            '-' => memory[pointer] -= 1,
-            '>' => pointer += 1,
-            '<' => pointer -= 1,
-            '.' => print!("{}", char::from(memory[pointer])),
+    while c < code_chars.len() {
+        match code_chars[c] {
+            '+' => memory[pointer] = memory[pointer].wrapping_add(1),
+            '-' => memory[pointer] = memory[pointer].wrapping_sub(1),
+            '>' => {
+                pointer += 1;
+                if pointer >= MEMORY_SIZE {
+                    panic!("pointer overflow");
+                }
+            }
+            '<' => {
+                if pointer == 0 {
+                    panic!("pointer underflow");
+                }
+                pointer -= 1;
+            }
+            '.' => {
+                print!("{}", char::from(memory[pointer]));
+            }
             ',' => memory[pointer] = std::io::stdin().bytes().next().unwrap().unwrap() as u8,
             '[' => {
                 if memory[pointer] == 0 {
                     let mut open_brackets = 1;
                     while open_brackets > 0 {
-                        pointer += 1;
-                        match code.chars().nth(pointer).expect("string index overflow") {
+                        c += 1;
+                        if c >= code_chars.len() {
+                            panic!("no matching bracket");
+                        }
+                        match code_chars[c] {
                             '[' => open_brackets += 1,
                             ']' => open_brackets -= 1,
                             _ => {}
                         }
                     }
+                } else {
+                    stack.push(c);
                 }
             }
             ']' => {
-                if memory[pointer] == 0 {
-                    let mut open_brackets = 1;
-                    while open_brackets > 0 {
-                        pointer -= 1;
-                        match code.chars().nth(pointer).expect("string index overflow") {
-                            '[' => open_brackets -= 1,
-                            ']' => open_brackets += 1,
-                            _ => {}
-                        }
+                if let Some(last_c) = stack.pop() {
+                    if memory[pointer] != 0 {
+                        c = last_c - 1;
+                        stack.push(last_c)
                     }
+                } else {
+                    panic!("no matching bracket");
                 }
             }
             _ => {}
         }
+        c += 1;
     }
 }
